31b294abe390d2f083011961e2785d0f928348a1
TechyPRO <jjosephttg@gmail.com>
2019-12-27 17:26:26 -0500

    Added Profile Logo and Animation to About

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import ProfileLogo from '../../components/ProfileLogo/ProfileLogo';
+
+import profilePicture from '../../assets/PortfolioSelfPic.jpg';
 
 import styles from './AboutPage.module.css';
 
 const AboutPage = _ => {
-    console.log(styles);
+    const currentText = "Click/Hover over one of the highlighted letters in my name to learn more about me!"
     return (
-        <h1 className={styles.Title}>Josh Joseph</h1>
+        <Fragment>
+            <section>
+                <ProfileLogo className={styles.SelfPicture} source={profilePicture} size="10rem"/>
+                <h1 className={styles.Title}>Joshua Joseph</h1>
+                <div className={styles.Divider} />
+                {//<p className={styles.Description}>{currentText}</p>
+                }
+            </section>   
+        </Fragment>
     );
 }
 
