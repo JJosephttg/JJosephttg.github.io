dcf7ec1b3bcd873607d8f936df4ccb79dff8fa10
TechyPRO <jjosephttg@gmail.com>
2020-01-23 13:14:26 -0500

    Styled Contact Page Further

--- src/containers/ContactPage/ContactPage.js
+++ src/containers/ContactPage/ContactPage.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import styles from './ContactPage.module.css';
 
 const ContactPage = _ => (
-    <div className={styles.ContactContainer}>
-        <h1>Contact Me!</h1>
-        <p>Email: jjosephttg@gmail.com</p>
-        <p>Phone Number: 1-810-874-4904</p>
-    </div>
+    <Fragment>
+        <h1 className={styles.Title}>Contact</h1>
+        <div className={styles.Divider}/>
+        <div className={styles.ContactContainer}>
+            <h1>Contact Me!</h1>
+            <p>Email: <a className={styles.ContactLink} href="mailto:jjosephttg@gmail.com">jjosephttg@gmail.com</a></p>
+            <p>Phone Number: <a className={styles.ContactLink} href="tel:+18108744904">1-810-874-4904</a></p>
+        </div>
+    </Fragment>
 );
 
 export default ContactPage;
